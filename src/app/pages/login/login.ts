d1f5629114c362fa362e0b33a08973ad2c52cf4f
byzg <byzg00@gmail.com>
2018-08-28 02:47:16 +0300

    Icons and styles

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,22 +1,29 @@
 import { Component} from '@angular/core';
 import { NavController, MenuController } from 'ionic-angular';
 
+import { SessionForm } from '../../forms';
+import { Session } from '../../resourses/factories';
+import { BaseCollection } from '../../resourses/collections';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
   path = 'pages.login';
+  sessionForm: SessionForm;
 
   constructor(
     public nav: NavController,
     public menu: MenuController,
+    public session: Session,
   ) {
     this.menu.swipeEnable(false);
+    this.sessionForm = new SessionForm(this.session, new BaseCollection<Session>());
   }
 
   login() {
-    // this.nav.setRoot(HomePage);
+    // this.sessionForm.save().then(() => this.nav.setRoot(HomePage));
   }
 
 }
