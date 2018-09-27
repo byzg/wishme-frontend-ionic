bf97ed55544c0304bc15210b878acdd14e00716e
byzg <byzg00@gmail.com>
2018-09-28 01:36:26 +0300

    fixes for deploy

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -2,11 +2,11 @@
 git branch -D gh-pages
 git push origin --delete gh-pages
 git checkout -b gh-pages
-ionic build
-find . -type d ! -path './www*' ! -path './node_modules*' ! -path './.git*' ! -path '.' | xargs rm -rf
+cat src/environments/environment.prod.ts > src/environments/environment.ts
+ionic cordova build browser --prod
+find . -type d ! -path './www*' ! -path './node_modules*' ! -path './platforms*' ! -path './.git*' ! -path '.' | xargs rm -rf
 rm -r  *.*
-mv www/* .
-rm -rf www
+mv platforms/browser/* .
 git add .
 git commit -m "Publishing to github pages"
 git push origin gh-pages
