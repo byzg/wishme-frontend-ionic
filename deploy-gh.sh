2be14a44492fbf330b25cbbd0b6c7a9e895cd943
byzg <byzg00@gmail.com>
2018-09-28 01:45:35 +0300

    fixes for deploy

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -6,7 +6,7 @@ cat src/environments/environment.prod.ts > src/environments/environment.ts
 ionic cordova build browser --prod
 find . -type d ! -path './www*' ! -path './node_modules*' ! -path './platforms*' ! -path './.git*' ! -path '.' | xargs rm -rf
 rm -r  *.*
-mv platforms/browser/www* .
+mv platforms/browser/www/* .
 git add .
 git commit -m "Publishing to github pages"
 git push origin gh-pages
