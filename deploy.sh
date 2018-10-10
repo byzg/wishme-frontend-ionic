93da50c4b5758a83529940bb452b2f467c635d89
byzg <byzg00@gmail.com>
2018-10-10 23:39:05 +0300

    workbox2 (3)

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git branch -D deploy
 git checkout -b deploy
 cat src/environments/environment.prod.ts > src/environments/environment.ts
 rm -fr www
-npm run ruild
+npm run build
 firebase deploy
 git add .
 git commit -m "deploy"
