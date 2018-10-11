f710a4e558610fefbea7bc833fb40ac652b15b74
byzg <byzg00@gmail.com>
2018-10-12 02:16:25 +0300

    move SW to main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,6 +6,13 @@ import { AppModule } from './app.module';
 
 if (environment.production) {
   enableProdMode();
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(reg) {
+        console.log('service worker registered: ', reg)
+      })
+      .catch(function(err) { console.error('[SW ERROR]', err) });
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
