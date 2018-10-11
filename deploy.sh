90c82b9c51f1279626987954b927178fa7bf0ca8
byzg <byzg00@gmail.com>
2018-10-12 00:01:01 +0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-BRANCH=`git branch | grep \* | cut -d ' ' -f2`
-git branch -D deploy
-git checkout -b deploy
 cat src/environments/environment.prod.ts > src/environments/environment.ts
 rm -fr www
 ionic build --prod
 firebase deploy
-git add .
-git commit -m "deploy"
-git checkout $BRANCH
+git checkout src/environments/environment.ts
